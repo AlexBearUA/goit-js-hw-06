ed4292196bb0eba230311a7b20bffea08c691faa
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-01-31 21:51:44 +0100

    +2

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
-const listWithCategoriesEl = document.querySelectorAll(".item");
-console.log("Number of categories:", listWithCategoriesEl.length);
-listWithCategoriesEl.forEach((item) => {
+const allCategoriesEl = document.querySelectorAll(".item");
+console.log("Number of categories:", allCategoriesEl.length);
+allCategoriesEl.forEach((item) => {
   console.log("Category:", item.firstElementChild.textContent);
   console.log("Elements:", item.querySelectorAll("li").length);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listOfIngredientsEl = document.querySelector("ul#ingredients");
+
+const makeListOfIngredients = (ingredients) => {
+  return ingredients.map((ingridient) => {
+    const ingredientsListItem = document.createElement("li");
+    ingredientsListItem.classList.add("item");
+    ingredientsListItem.textContent = ingridient;
+    return ingredientsListItem;
+  });
+};
+
+const elements = makeListOfIngredients(ingredients);
+listOfIngredientsEl.append(...elements);
