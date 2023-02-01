a3fe02366d6d9051a3ed23a3ddb83a85c39d5c36
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-02-01 21:54:49 +0100

    +10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesEl = document.querySelector("#boxes");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const input = document.querySelector("input");
+
+const makeBoxesMarkup = () => {
+  const createBoxes = (amount) => {
+    const boxes = new Array(amount).fill("");
+    console.log(boxes);
+    return boxes.reduce((resultString, element, index, array) => {
+      return (resultString += `<div style = "width: ${
+        30 + index * 10
+      }px; height: ${
+        30 + index * 10
+      }px; background-color: ${getRandomHexColor()}"></div>`);
+    }, ``);
+  };
+  boxesEl.insertAdjacentHTML("beforeend", createBoxes(Number(input.value)));
+};
+
+const removeBoxes = () => {
+  boxesEl.innerHTML = "";
+};
+
+createBtn.addEventListener("click", makeBoxesMarkup);
+destroyBtn.addEventListener("click", removeBoxes);
