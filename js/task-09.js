363fa2ee2d569397308535bdad3a837f13dfa268
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-02-01 18:29:20 +0100

    +9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const backgroundColor = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+const changeColorBtnClickHeandler = () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  backgroundColor.textContent = `${getRandomHexColor()}`;
+};
+
+changeColorBtn.addEventListener("click", changeColorBtnClickHeandler);
